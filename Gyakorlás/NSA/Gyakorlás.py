893991ff38da0c43094b07534aac0646ab21af73
MatePatrik <matepatrik2002@gmail.com>
2023-01-09 01:10:44 +0100

    KAR

--- Gyakorlás/NSA/Gyakorlás.py
+++
@@ -1,8 +0,0 @@
-szam1=int(input("Ird be az egyik számot: "))
-szam2=int(input("Ird be a másik számot: "))
-if szam1>szam2:
-    print(szam1,"nagyobb mint ",szam2)
-else:
-    print(szam2,"nagyobb mint ",szam1)
-
-
